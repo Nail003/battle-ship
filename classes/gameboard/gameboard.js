03d28f6318b0dd629dd119a45f8d5fc1fe08817b
Nail003 <danialzafarstudy003@gmail.com>
2024-06-22 18:41:15 +0500

    feat: implement function to add new ships on the board

--- classes/gameboard/gameboard.js
+++ classes/gameboard/gameboard.js
@@ -1,7 +1,11 @@
+const rowsArray = "abcdefghij".split("");
+
 export default class Gameboard {
     #board;
+    #border;
     constructor() {
         this.#board = this.#createCoords();
+        this.#border = 10;
     }
 
     info(coords) {
@@ -9,24 +13,36 @@ export default class Gameboard {
         return this.#board[row][col];
     }
 
-    addShip(coords, ship) {
+    addShip(coords, ship, horizontal = false) {
         const [row, col] = coords;
 
-        // Add ship to specified cell
-        this.#board[row][col].isEmpty = false;
-        this.#board[row][col].shipRef = ship;
+        if (horizontal) {
+            // Don't add ships that exeeds border horizontly
+            const rowIndex = rowsArray.indexOf(row);
+            if (rowIndex + ship.length > this.#border) return false;
+
+            // Add ship
+            this.#addShipHorizontly(row, col, ship);
+            return true;
+        }
+        // Don't add ships that exeeds border verticaly
+        if (col + ship.length > this.#border) return false;
+
+        // Add ship
+        this.#addShipVerticaly(row, col, ship);
+        return true;
     }
 
     // Private Methods
     #createCoords() {
         const coords = {};
         // Create rows from a to j (total 10)
-        const rows = "abcdefghij".split("");
+        const rows = rowsArray;
         for (const row of rows) {
             // Assign an array to each row
             coords[row] = [];
             // Add columns (total 10)
-            for (let col = 1; col < 11; col++) {
+            for (let col = 0; col < 10; col++) {
                 // Base cell of the board
                 const cell = { isEmpty: true, shipRef: null };
                 // Add base cell
@@ -34,5 +50,26 @@ export default class Gameboard {
             }
         }
         return coords;
+    }
+
+    #addShipHorizontly(row, col, ship) {
+        // Add ship to specified cells
+        // We need to get the numerical index of row string
+        const rowIndex = rowsArray.indexOf(row);
+        for (let i = 0; i < ship.length; i++) {
+            // Use the numerical value to find the corresponding string value
+            const nextRow = rowsArray[rowIndex + i];
+            this.#board[nextRow][col].isEmpty = false;
+            this.#board[nextRow][col].shipRef = ship;
+        }
+    }
+
+    #addShipVerticaly(row, col, ship) {
+        // Add ship to specified cells
+        for (let i = 0; i < ship.length; i++) {
+            const nextCol = col + i;
+            this.#board[row][nextCol].isEmpty = false;
+            this.#board[row][nextCol].shipRef = ship;
+        }
     }
 }
--- classes/gameboard/gameboard.test.js
+++ classes/gameboard/gameboard.test.js
@@ -14,12 +14,73 @@ describe("Gameboard", () => {
         expect(board.info(["c", 3])).toEqual(defaultCell);
     });
 
-    it("should add ship to specified coords", () => {
-        const board = new Gameboard();
-        const ship = new Ship();
-        const coords = ["c", 3];
+    describe("Add ships to specified coords", () => {
+        test("ship with length = 1", () => {
+            const board = new Gameboard();
+            const ship = new Ship();
+            const cell = { isEmpty: false, shipRef: ship };
+            const coords = ["c", 3];
 
-        board.addShip(coords, ship);
-        expect(board.info(coords)).toEqual({ isEmpty: false, shipRef: ship });
+            board.addShip(coords, ship);
+            expect(board.info(coords)).toEqual(cell);
+        });
+
+        test("ship just at border", () => {
+            const board = new Gameboard();
+            const ship = new Ship();
+            const cell = { isEmpty: false, shipRef: ship };
+            const coords = ["j", 9];
+
+            board.addShip(coords, ship);
+            expect(board.info(coords)).toEqual(cell);
+        });
+
+        test("ship with length = 3", () => {
+            const board = new Gameboard();
+            const ship = new Ship(3, 3);
+            const cell = { isEmpty: false, shipRef: ship };
+            const coords = ["f", 5];
+            const coords2 = ["f", 6];
+            const coords3 = ["f", 7];
+
+            board.addShip(coords, ship);
+
+            expect(board.info(coords)).toEqual(cell);
+            expect(board.info(coords2)).toEqual(cell);
+            expect(board.info(coords3)).toEqual(cell);
+        });
+
+        test("ship with horizontal rotation", () => {
+            const board = new Gameboard();
+            const ship = new Ship(3, 3);
+            const cell = { isEmpty: false, shipRef: ship };
+            const coords = ["d", 5];
+            const coords2 = ["e", 5];
+            const coords3 = ["f", 5];
+
+            board.addShip(coords, ship, true);
+
+            expect(board.info(coords)).toEqual(cell);
+            expect(board.info(coords2)).toEqual(cell);
+            expect(board.info(coords3)).toEqual(cell);
+        });
+    });
+
+    it("should not add ship exeeding the borders of the board vertically", () => {
+        const board = new Gameboard();
+        const ship = new Ship(3, 3);
+        const coords = ["f", 8];
+
+        expect(board.addShip(coords, ship)).toBeFalsy();
+        expect(board.info(coords)).toEqual(defaultCell);
+    });
+
+    it("should not add ship exeeding the borders of the board horizontly", () => {
+        const board = new Gameboard();
+        const ship = new Ship(3, 3);
+        const coords = ["i", 8];
+
+        expect(board.addShip(coords, ship, true)).toBeFalsy();
+        expect(board.info(coords)).toEqual(defaultCell);
     });
 });
