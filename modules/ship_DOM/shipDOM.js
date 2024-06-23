f40bc263d8fc28a22416f6cc20e76a640b1aa63b
Nail003 <danialzafarstudy003@gmail.com>
2024-06-23 19:53:56 +0500

    feat: ships that break the rules are not rendered

--- modules/ship_DOM/shipDOM.js
+++ modules/ship_DOM/shipDOM.js
@@ -8,24 +8,24 @@ export default function renderShips(player) {
     const subMarine = new Ship(3, 3);
     const destroyer = new Ship(2, 2);
 
-    renderShip(player.board.addShip(["a", 1], carrier));
-    renderShip(player.board.addShip(["f", 1], battleShip));
-    renderShip(player.board.addShip(["c", 1], cruiser));
-    renderShip(player.board.addShip(["d", 5], subMarine));
-    renderShip(player.board.addShip(["j", 1], destroyer));
+    renderShip(player.board.addShip(["a", 1], carrier, true));
+    renderShip(player.board.addShip(["f", 1], battleShip, true));
+    renderShip(player.board.addShip(["c", 1], cruiser, true));
+    renderShip(player.board.addShip(["d", 5], subMarine, true));
+    renderShip(player.board.addShip(["j", 1], destroyer, true));
 }
 
 function renderShip(shipCoords) {
-    console.log(shipCoords);
-    const player1DOM = document.getElementsByClassName("player1-board")[0];
+    // If ship breaks the rules don't add it
+    if (!shipCoords) return;
 
+    const player1DOM = document.getElementsByClassName("player1-board")[0];
     const coordsString = shipCoords.reduce(
         (prev, curr) => (prev += curr.join("")),
         ""
     );
 
     const coords = coordsString.match(/.{1,2}/g);
-
     for (const coord of coords) {
         const cellDOM = player1DOM.querySelector(`[data-coords~=${coord}]`);
         cellDOM.classList.add("board-cell--ship");
